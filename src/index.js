a050ae1e7d41cb7561448499eec6725bbf6590ea
nojaja <free.riccia@gmail.com>
2023-05-22 00:02:26 +0900

    コメント追加

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ export class GenerateLib {
         }
         return _dirwalk(basepath, null, callback)
     }
+
     readflow(flowpath) {
         const fullpath = path.join(process.cwd(), flowpath)
         const flowContents = fs.readFileSync(fullpath);
@@ -40,6 +41,7 @@ export class GenerateLib {
                 const name = element.meta.module || element.name
                 subflowData[name] = { packageData: {}, subflows: {} }
 
+                // Create package.json definition information
                 const packageData = {
                     "name": name,
                     "version": subflows.meta.version || "0.0.1",
@@ -59,39 +61,33 @@ export class GenerateLib {
 
                 subflows.flow = []
                 for (const flow of subflowJSON) {
+
+                    //Convert the part using subflow to the calling definitions after library conversion
                     if (flow.type == `subflow:${subflows.id}`) {//flow.typeがsubflowsのIDと同じ場合は、そのsubflowsを使っている
                         flow.type = subflows.meta.type
+
+                        //インスタンス側の設定値取得
                         const _flowenv = {}
                         for (const env of flow.env || []) {
                             _flowenv[env.name] = { "type": env.type, "value": env.value} 
                         }
+
+                        //Sets the environment variables. If there is no value for an environment variable in the instance, the default value defined in the subflow will be used.
                         for (const prototype_env of subflows.env) {
                             const _env = _flowenv[prototype_env.name] || {type:null,value:null}
                             flow[prototype_env.name] = { "type": _env.type || prototype_env.type, "value": _env.value || prototype_env.value || "" }
                             //console.log("env: instance:",_env," prototype:",prototype_env)
                         }
                         delete flow.env
-
-                        // if (flow.env) {
-                        //     for (const env of flow.env) {
-                        //         //flow[env.name] = (env.type && env.type!="str") ? { "type": env.type, "value": env.value || "" } : env.value || ""
-                        //         //flow[env.name] = { "type": env.type, "value": env.value || "" } 
-                        //         flow[env.name] = { "type": env.type, "value": env.value || "" }
-                        //     }
-                        //     delete flow.env
-                        // }
-
                     }
+
+                    //Move definition belonging to subflow under subflow
                     if (flow.z == subflows.id) {//flow.zがsubflowsのIDと同じ場合は、そのsubflowsに属している
                         subflows.flow.push(flow)
                     }
                 }
                 subflowData[name].package = packageData
                 subflowData[name].subflows = subflows
-                //subflows
-                //      "id": "cd185f8f533e0f2d",
-                //      "type": "subflow",
-                ///"type": "subflow:cd185f8f533e0f2d",
             }
         }
         const subflowsid = {}
@@ -112,12 +108,12 @@ export class GenerateLib {
         const flowJSON = JSON.parse(JSON.stringify(subflowJSON))
         //subflowに属してないflowのみにする
         resultData["flowData"] = flowJSON.filter(element => !(subflowsid[element.z] || element.type == "subflow"));
-        //console.log(result)
+        
         return resultData
     }
 
+    //Output a file based on analysis results
     generate(resultData) {
-
         const subflowData = resultData["subflowData"]
         const flowData = resultData["flowData"]
         //subフローを削除したflows.jsonを出力
@@ -125,7 +121,11 @@ export class GenerateLib {
         if (!fs.existsSync(outputdir)) {
             fs.mkdirSync(outputdir, { recursive: true });
         }
+
+        //Output flow.json excluding subflow elements
         fs.writeFileSync(path.join(outputdir, 'flows.json'), JSON.stringify(flowData, null, '    '));
+        
+        //Output for each subflow
         for (const key in subflowData) {
             const subflows = subflowData[key];
             const outputsubflowdir = path.join(process.cwd(), 'output', key, 'subflow')
@@ -134,20 +134,21 @@ export class GenerateLib {
                 fs.mkdirSync(outputsubflowdir, { recursive: true });
             }
 
+            // output subflow.json
             fs.writeFileSync(path.join(outputsubflowdir, `${key}.json`), JSON.stringify(subflows.subflows, null, '    '))
+            
+            // output package.json
             fs.writeFileSync(path.join(outputdir, 'package.json'), JSON.stringify(subflows.package, null, '    '))
+
+            // output index.js
             const index_js = fs.readFileSync(path.join(process.cwd(), 'template', 'index.js'), 'utf-8')
             fs.writeFileSync(path.join(outputdir, 'index.js'), index_js)
 
+            // output README.md
             const README_md = fs.readFileSync(path.join(process.cwd(), 'template', 'README.md'), 'utf-8')
             const template = Handlebars.compile(README_md)
             fs.writeFileSync(path.join(outputdir, 'README.md'), template(subflows.package))
         }
-
-        //const templatedir = path.join(process.cwd(), "template")
-        //this.dirwalk(templatedir, (basepath, currentpath, file) => {
-        //    console.log("dirwalk:", basepath, currentpath, file)
-        //})
     }
 
 }
@@ -155,15 +156,15 @@ export class GenerateLib {
 
 commander
     .version('0.0.1')
-    .requiredOption('-f, --file <path>', 'File to upload')
+    .requiredOption('-f, --file <path>', 'flow.json')
     .parse(process.argv);
 
 console.log(commander.opts())
 
 const options = commander.opts();
 
-const test = new GenerateLib()
+const gen = new GenerateLib()
 
 
-const resultData = test.readflow(options.file)
-test.generate(resultData)
+const resultData = gen.readflow(options.file)
+gen.generate(resultData)
